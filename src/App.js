f0011acb4a8c038cb0df3d4f8342a41dece66cf8
emre <emreyigit1997@gmail.com>
2022-11-06 21:54:30 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppContextProvider } from "./components/contexts/AppContext";
+import Loader from "./components/UI/atoms/Loader";
+import DetailsView from "./components/views/DetailsView"
+import ListView from "./components/views/ListView"
 
-const ListView = React.lazy(() => import("./components/views/ListView"))
-const DetailsView = React.lazy(() => import("./components/views/DetailsView"))
+// const ListView = React.lazy(() => import("./components/views/ListView"))
+// const DetailsView = React.lazy(() => import("./components/views/DetailsView"))
 
 function App() {
   return (
     <BrowserRouter>
       <AppContextProvider>
-        <Routes>
-          <Route path='/details/:title' element={<DetailsView />} />
-          <Route path='/' element={<ListView />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/details/:title' element={<DetailsView />} />
+            <Route path='/' element={<ListView />} />
+          </Routes>
+        </Suspense>
       </AppContextProvider>
     </BrowserRouter>
 
--- src/components/UI/molecules/ItemCard/index.jsx
+++ src/components/UI/molecules/ItemCard/index.jsx
@@ -1,12 +1,10 @@
-import React, { useCallback } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Link, } from 'react-router-dom'
 import styles from './itemcard.module.scss'
 
 const ItemCard = ({
   item
 }) => {
-
-  const navigate = useNavigate()
 
   const renderItem = (text, field) => {
     return (
